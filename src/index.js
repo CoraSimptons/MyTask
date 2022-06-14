990905002e4b8d7ada7a999becf5cbd55f3a0580
pseudoname <nnmt.lfg.2020@gmail.com>
2022-06-15 01:09:46 +0700

    Classify categories for home page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const {engine} = require('express-handlebars')
 const methodOverride = require('method-override')
+const moment = require('moment');
 const path = require('path')
 const app = express()
 const port = 3000
@@ -32,6 +33,28 @@ app.engine(
     extname: '.hbs',
     helpers: {
       sum: (a, b) => a + b,
+      standardDate: (date) => {
+        let text = new Date(date)
+        let month = parseInt(text.getMonth()) + 1
+        month = month > 9 ? month : '0' + month
+        let day = parseInt(text.getDate())
+        day = day > 9 ? day : '0' + day
+        return  text.getFullYear() + '-' + month + '-' + day
+      },
+      formatDate: (date) => {
+        let text = new Date(date)
+        let month = parseInt(text.getMonth()) + 1
+        month = month > 9 ? month : '0' + month
+        let day = parseInt(text.getDate())
+        day = day > 9 ? day : '0' + day
+        return  day + '-' + month + '-' + text.getFullYear()
+      },
+      compareDates: (date) => {
+        if (moment(date).isAfter(new Date())) {
+          return true;
+        }
+        return false;
+      },
     }
   })
 );
